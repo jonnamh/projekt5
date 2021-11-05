1ddfde5de1e15c1b09ee423c6dcee272f61eef3d
graffsussi <graffsussi@gmail.com>
2021-11-05 11:54:04 +0100

    JavaScript og CSS ændringer
    Jeg har kodet en alert til Tilmeld i footeren ved nyhedsbrevet. Derudover har jeg gjort teksten på Støt knappen i menuen til hvid, som har irriteret mig forevigt. Og så har jeg tilføjet grafik til nederst oå forsiden og centreret.

---
+++ js/nyhedsbrev.js
@@ -0,0 +1,5 @@
+
+console.log("virker");
+function pindsvin() {
+    alert("Tak fordi du har tilmeldt dig vores nyhedsbrev. Vi ser frem til at kunne dele vores spændende nyheder med dig. Du kan til hver tid afmelde dig igen.") 
+}
