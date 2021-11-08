575fe1bd1a908afdd6737aed818f90caa55e7df5
jonnamh <jonna.mh@hotmail.com>
2021-11-08 11:02:36 +0100

    fikset js på footer i alle sider

--- js/nyhedsbrev.js
+++ js/nyhedsbrev.js
@@ -1,5 +1,4 @@
-
 console.log("virker");
 function pindsvin() {
-    alert("Tak fordi du har tilmeldt dig vores nyhedsbrev. Vi ser frem til at kunne dele vores spændende nyheder med dig. Du kan til hver tid afmelde dig igen.") 
+    alert("Tak fordi du har tilmeldt dig vores nyhedsbrev. Vi ser frem til at kunne dele vores spændende nyheder med dig. Du kan til hver tid afmelde dig igen.")
 }
