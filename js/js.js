830155682356e7843b162fd10d2edd63a2169a87
jonnamh <jonna.mh@hotmail.com>
2021-11-09 09:29:46 +0100

    lavet betaling, og sidste js

--- js/js.js
+++ js/js.js
@@ -2,3 +2,12 @@ document.getElementById("send").addEventListener("click", function() {
   document.querySelector(".row2").style.display = "flex";
   document.querySelector(".row").style.display = "none";
 })
+
+var minGruppe=['Sussi', 'Xenia', 'Nanna', 'Oliver', 'Jonna'];
+
+  if (minGruppe.length = 5) {
+    console.log("Gruppen består af " + minGruppe.length + " medlemmer");
+
+  } else {
+    console.log("For få medlemmer, skaf en ekstra!");
+}
